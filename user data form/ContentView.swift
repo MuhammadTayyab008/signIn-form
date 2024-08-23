0c4d3e2257d786e725631aaea6c1137b3afd4f96
Muhammad Tayyab <maliktayyabbro@gmail.com>
2024-08-23 17:50:21 +0500

    sign fields

--- user data form/ContentView.swift
+++ user data form/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("user name", text: $firstName)
+                .padding()
+                .frame(width: 200, height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0))
+                )
+            
+            TextField("user name", text: $lastName)
+                .padding()
+                .frame(width: 200, height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0))
+                )
+            
+            TextField("user name", text: $email)
+                .padding()
+                .frame(width: 200, height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0))
+                )
+            
+            TextField("user name", text: $password)
+                .padding()
+                .frame(width: 200, height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.black, style: StrokeStyle(lineWidth: 1.0))
+                )
+            HStack {
+                Button(action:
+                        {print("button pressed")},
+                       label: {
+                    Text("Cancel")
+                    
+                })
+                Spacer()
+                
+                Button(action:
+                        {print("button pressed")},
+                       label: {
+                    Text("Sign in")
+                    
+                    
+                })
+                .padding(.leading)
+            }
+            .frame(width: 200, height: 40)
+            
         }
-        .padding()
+        
     }
 }
 
